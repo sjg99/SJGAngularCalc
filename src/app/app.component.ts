f8413d2344639eb20ec922fdb058dc5986d8b40c
sjg99 <38005341+sjg99@users.noreply.github.com>
2019-08-20 23:10:04 -0500

    Reset Calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,46 +9,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SJGAngularCalc';
-  Result='';
-  Operation:string='';
-  Operator:string='';
-  Number1='';
-  Number2='';
-  showcalc=true;
-  showres=true;
+  Result = '';
+  Operation: string = '';
+  Operator: string = '';
+  Number1 = '';
+  Number2 = '';
+  showcalc = true;
+  showres = true;
 
-  OperatorChange(){
-    if(this.Operation!=''){
-      this.showcalc=false;
-      if(this.Operation=='Addition')this.Operator='+';
-      if(this.Operation=='Substraction')this.Operator='-';
-      if(this.Operation=='Multiplication')this.Operator='*';
-      if(this.Operation=='Division')this.Operator='/';
-      if(this.Operation=='Module')this.Operator='%';
+  OperatorChange() {
+    if (this.Operation != '') {
+      this.showcalc = false;
+      if (this.Operation == 'Addition') this.Operator = '+';
+      if (this.Operation == 'Substraction') this.Operator = '-';
+      if (this.Operation == 'Multiplication') this.Operator = '*';
+      if (this.Operation == 'Division') this.Operator = '/';
+      if (this.Operation == 'Module') this.Operator = '%';
     }
   }
-  Calculate(){
-    this.showres=false;
-    if((this.Number1 && this.Number2) || (this.Number1=='0' && this.Number2=='0') || (this.Number1 && this.Number2=='0') || (this.Number1=='0' && this.Number2)){
-      if(this.Operator=='+'){
-        this.Result=(parseFloat(this.Number1)+ parseFloat(this.Number2)).toString();
+  Calculate() {
+    this.showres = false;
+    if ((this.Number1 && this.Number2) || (this.Number1 == '0' && this.Number2 == '0') || (this.Number1 && this.Number2 == '0') || (this.Number1 == '0' && this.Number2)) {
+      if (this.Operator == '+') {
+        this.Result = (parseFloat(this.Number1) + parseFloat(this.Number2)).toString();
       }
-      if(this.Operator=='-'){
-        this.Result=(parseFloat(this.Number1)- parseFloat(this.Number2)).toString();
+      if (this.Operator == '-') {
+        this.Result = (parseFloat(this.Number1) - parseFloat(this.Number2)).toString();
       }
-      if(this.Operator=='*'){
-        this.Result=(parseFloat(this.Number1)* parseFloat(this.Number2)).toString();
+      if (this.Operator == '*') {
+        this.Result = (parseFloat(this.Number1) * parseFloat(this.Number2)).toString();
       }
-      if(this.Operator=='/'){
-        if(this.Number2!='0')this.Result=(parseFloat(this.Number1)/ parseFloat(this.Number2)).toString();
-        else this.Result="You can´t divide by 0";
+      if (this.Operator == '/') {
+        if (this.Number2 != '0') this.Result = (parseFloat(this.Number1) / parseFloat(this.Number2)).toString();
+        else this.Result = "You can´t divide by 0";
       }
-      if(this.Operator=='%'){
-        this.Result=(parseFloat(this.Number1)% parseFloat(this.Number2)).toString();
+      if (this.Operator == '%') {
+        this.Result = (parseFloat(this.Number1) % parseFloat(this.Number2)).toString();
       }
+    } else {
+      this.Result = "Both Fields must have a value";
     }
-    else{      
-      this.Result="Both Fields must have a value";
-    }
+  }
+  Reset(){
+    this.Result = '';
+    this.Operation = '';
+    this.Operator='';
+    this.Number1 = '';
+    this.Number2 = '';
+    this.showcalc = true;
+    this.showres = true;
   }
 }
