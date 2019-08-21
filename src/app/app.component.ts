fa91918ff81b51129fd5782ac3a82e793a500653
sjg99 <38005341+sjg99@users.noreply.github.com>
2019-08-20 22:36:57 -0500

    Basic SetUp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SJGAngularCalc';
+  Result='';
+  Operation:string='';
+  Operator:string='';
+  Number1='';
+  Number2='';
+  showcalc=true;
+  showres=true;
+
+  OperatorChange(){
+    if(this.Operation!=''){
+      this.showcalc=false;
+      if(this.Operation=='Addition')this.Operator='+';
+      if(this.Operation=='Substraction')this.Operator='-';
+      if(this.Operation=='Multiplication')this.Operator='*';
+      if(this.Operation=='Division')this.Operator='/';
+      if(this.Operation=='Module')this.Operator='%';
+    }
+  }
+  Calculate(){
+    if(this.Number1!='' && this.Number2!=''){
+
+    }
+    else{
+      this.showres=false;
+      this.Result="Both Fields must have a value";
+    }
+  }
 }
